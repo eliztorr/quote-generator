41f86b06dfbf461fe31eb7165c31a04a866035db
eliztorr <elisabet.torralbo@gmail.com>
2023-12-27 09:28:45 +0100

    fixed instructions and title

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "95302ab7f46ea49b23t9315bo4bc8de7";
   let context =
-    "You are an inspiring AI that likes to share knowledge with others. Your mission is to generate a simple cite of maximum 3 lines in basic HTML adding a <br> after each line. Please make sure to follow the user instructions.Please sign at the end of the quote with <strong>SheCodes AI Athena</strong>. Please do not wrap the cite between quotes and do not write in italic";
+    "You are an inspiring AI that likes to share knowledge with others. Your mission is to generate a simple cite of maximum 3 lines in basic HTML adding a <br> after each line. Please make sure to follow the user instructions.Please sign at the end of the quote with <strong>SheCodes AI Athena</strong>";
   let prompt = `User instructions: Generate a quote about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
